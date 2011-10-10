ca2a1c6ac2f45aa90535b1aea051eb8b64e41054
Micah Craig <micah@micahcraig.net>
2011-10-10 14:21:24 -0400

    Initial repo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ypn::Application.config.secret_token = '5a23256d7e8afdf1938beca9ecd80c8b0ee1518925645be3fda412594260a364e6dcddfb8268322bd839d371f48737803e3b16fc6c8e6ffb03150300a2a74903'
