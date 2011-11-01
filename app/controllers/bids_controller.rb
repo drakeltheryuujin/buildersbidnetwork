ade5221666dccd7083c5fe6a812de5bd4ecf8716
Micah Craig <micah@micahcraig.net>
2011-11-01 16:21:43 -0400

    bid display/edit smarter project bids display dynamic bid calculations

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -3,6 +3,12 @@ class BidsController < ApplicationController
   
   def show
     @bid = Bid.find(params[:id])
+    @project = @bid.project
+  end
+  
+  def edit
+    @bid = Bid.find(params[:id])
+    @project = @bid.project
   end
   
   def new
@@ -11,25 +17,30 @@ class BidsController < ApplicationController
     @project.line_items.each do |line_item|
       @bid.line_item_bids.build :line_item => line_item
     end
-    #3.times do
-    #  @bid.line_item_bids.build
-    #end
   end
   
   def create
     @project = Project.find(params[:project_id])
-    #@bid = @project.bids.create(params[:bid])
     @bid = @project.bids.new(params[:bid])
-    #@bid = Bid.new(params[:bid])
-    #@bid.project = project
     
     @bid.user_id = current_user.id
     
     if @bid.save
-      #redirect_to([@project, @bid], :notice => 'Bid was successfully created.')
       redirect_to(project_path(@project), :notice => 'Bid was successfully created.')
     else
       render :action => "new"
+    end
+  end
+  
+  def update
+    @bid = Bid.find(params[:id])
+    
+    @bid.user_id = current_user.id
+    
+    if @bid.update_attributes(params[:bid])
+      redirect_to(project_path(@bid.project), :notice => 'Bid was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 end
