a552388c543ddb5e19ca24971e1308e3f70e4998
Micah Craig <micah@micahcraig.net>
2012-02-24 13:43:57 -0500

    test data

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,9 @@ Ypn::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
   
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.asset_host = 'http://localhost:3000'
+
   PAPERCLIP_STORAGE_OPTIONS = {}
   
   config.after_initialize do
