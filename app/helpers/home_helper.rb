4b545a564f83ce78b09b0e6cd172cefa557a6565
Micah Craig <micah@micahcraig.net>
2012-02-28 15:47:01 -0500

    live numbers for homepage dashboard

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,20 @@ module HomeHelper
   def put_if_current_page(output, path)
     current_page?(path) ? output : ''
   end
+
+  def projects_posted_in_days_count(days = 30)
+    Project.where(:state => :published.to_s).where("updated_at > :then", :then => (Time.now - days.days)).count
+  end
+
+  def projects_awarded_in_days_count(days = 30)
+    Bid.where(:state => :accepted.to_s).where("updated_at > :then", :then => (Time.now - days.days)).count
+  end
+
+  def users_projects_count(user = current_user)
+    Project.where(:user_id => user.id).count
+  end
+
+  def bids_on_users_projects_count(user = current_user)
+    Bid.joins(:project).where(:projects => {:user_id => user.id}).count
+  end
 end
