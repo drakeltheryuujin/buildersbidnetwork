226fcffa57a87bcbc44eb33d7bf098a7af1e6c90
Micah Craig <micah@micahcraig.net>
2012-03-03 13:15:13 -0500

    users, profiles, projects and line items in fixtures

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -1,29 +1,22 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "credit value validated against estimated budget" do
+    p = Project.new(
+      :name => 'test project', 
+      :project_type_id => 1,
+      :user => users(:one),
+      :bidding_end => Time.now + 2.weeks,
+      :project_start => Date.today + 3.weeks,
+      :project_end => Date.today + 4.weeks,
+      :description => "testtest",
+      :state => :published.to_s,
+      :credit_value => 1,
+      :estimated_budget => nil,
+      :location_attributes => {:address1 => '97 Bond St', :city => 'Brooklyn', :state => 'NY', :post_code => '11217'},
+      :line_items_attributes => [{:content => "content", :units => 1}])
+    p.save
+    y p.errors
+    assert false
   end
 end
-# == Schema Information
-#
-# Table name: projects
-#
-#  id              :integer         not null, primary key
-#  name            :string(255)     not null
-#  user_id         :integer         not null
-#  created_at      :datetime
-#  updated_at      :datetime
-#  bidding_end     :datetime        not null
-#  pre_bid_meeting :datetime
-#  project_start   :date            not null
-#  project_end     :date            not null
-#  description     :text            not null
-#  notes           :text
-#  location_id     :integer         not null
-#  project_type_id :integer         not null
-#  latitude        :float
-#  longitude       :float
-#
-
