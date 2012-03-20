049d8559da703fee00ed3277d103c5b965adf7f8
Dominic Tancredi <dom@domandtom.com>
2012-03-20 19:34:39 -0400

    remove comments

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,6 @@ class SearchController < ApplicationController
   helper :all
 
   def index
-    #@conversations = Kaminari.paginate_array(@mailbox.inbox).page(params[:page]).per(9)
-    #Kaminari.paginate_array(my_array_object).page(params[:page]).per(10)
-    #@search = Search.new(params[:search])
-    #@search = Kaminari.paginate_array(Search.new(params[:search]).results).page(params[:page]).per(9)
     @params = params[:search]
     if params[:page].blank?
       @params['page'] = 1
