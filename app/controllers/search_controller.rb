be31ee27c94cc17c8d346184b152e279601ac9c4
Dominic Tancredi <dom@domandtom.com>
2012-03-20 18:07:43 -0400

    pagination logic, rendering, and style for search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,18 @@ class SearchController < ApplicationController
   helper :all
 
   def index
-    @search = Search.new(params[:search])
+    #@conversations = Kaminari.paginate_array(@mailbox.inbox).page(params[:page]).per(9)
+    #Kaminari.paginate_array(my_array_object).page(params[:page]).per(10)
+    #@search = Search.new(params[:search])
+    #@search = Kaminari.paginate_array(Search.new(params[:search]).results).page(params[:page]).per(9)
+    @params = params[:search]
+    if params[:page].blank?
+      @params['page'] = 1
+    else
+      @params['page'] = params[:page]
+    end
+    
+    @search = Search.new(@params)
   end
 
 end
