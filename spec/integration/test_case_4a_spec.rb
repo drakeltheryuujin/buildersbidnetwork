9ddaa8c0f00c03c40b95677341e2ec41cf22bb7a
mobrien <michael@domandtom.com>
2012-03-26 13:05:52 -0500

    Revised Code for Test Case 4a

--- spec/integration/test_case_4a_spec.rb
+++ spec/integration/test_case_4a_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'Test Case 4 Test Viewing of Profie' do
 fixtures:all
-  it 'Verifies the User can Successfully purchase a Subscription' do
+  it 'Verifies the User can Successfully View a Profile' do
     visit '/'
     fill_in 'user_email', :with => 'user@developera.com'
     find_field('user_email').value.should == 'user@developera.com' 
@@ -10,8 +10,9 @@ fixtures:all
     find_field('Password').value.should == 'user@developera.com' 
     click_button 'Login'
     page.should have_content('Dashboard')
-    click_button "Company Overview"
+    click_link "Profile"
+    click_link "Company Overview"
     page.should have_content('Company Description')
-    click_button "Projects Posted"
+    click_link "Posted Projects"
     end
 end
