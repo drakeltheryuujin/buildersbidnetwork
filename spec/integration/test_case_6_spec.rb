8ade3b5664f91e714f7f9d221d899ed87827e25a
mobrien <michael@domandtom.com>
2012-03-27 14:15:59 -0500

    Revised Code for Test Case 6 now that Subscription and Credits are properly granted.

--- spec/integration/test_case_6_spec.rb
+++ spec/integration/test_case_6_spec.rb
@@ -10,33 +10,20 @@ fixtures:all
     find_field('Password').value.should == 'user@contractori.com' 
     click_button 'Login'
     page.should have_content('Dashboard')
-    click_link "Credits"
-    page.find(:xpath, './/a[@href="/credits/new?package=0"]').click
-    select('Visa', :from => 'payment_credit_card_type')
-    fill_in 'payment_credit_first_name', :with => 'Test'
-    fill_in 'payment_credit_last_name', :with => 'Credits'
-    fill_in 'payment_credit_card_number', :with => '4111111111111111'
-    fill_in 'payment_credit_card_verification', :with => '111'
-    fill_in 'payment_credit_card_billing_zip', :with => '60126'
-    select('2013', :from => 'payment_credit_card_expires_on_1i')
-    select('6 - June', :from => 'payment_credit_card_expires_on_2i')
-    click_button "Submit Payment"
-    find('.alert-message').should have_content('Thank you for your payment.')
-   
-    click_link "Credits"
-    click_link "Buy a Subscription"
-    page.find(:xpath, './/a[@href="/subscriptions/new?package=monthly"]').click
-    select('Visa', :from => 'subscription_payment_card_type')
-    fill_in 'subscription_payment_first_name', :with => 'Test'
-    fill_in 'subscription_payment_last_name', :with => 'Subscription'
-    fill_in 'subscription_payment_card_number', :with => '4111111111111111'
-    fill_in 'subscription_payment_card_verification', :with => '111'
-    fill_in 'subscription_payment_card_billing_zip', :with => '60126'
-    select('2013', :from => 'subscription_payment_card_expires_on_1i')
-    select('6 - June', :from => 'subscription_payment_card_expires_on_2i')
-    click_button "Submit Payment"
-    find('.alert-message').should have_content('Thank you for your payment.')
-      
+    
+     /* Test Case 1 (User Fills out Invalid properties for Bid; Bid is not saved/published) /*
+    click_link "Browse"
+    click_link "Developer B Project 5"
+    click_link "Enter Bid"
+    fill_in 'bid_line_item_bids_attributes_0_unit_cost', :with => 'x'
+    fill_in 'bid_line_item_bids_attributes_1_unit_cost', :with => 'x'
+    fill_in 'bid_line_item_bids_attributes_2_unit_cost', :with => 'x'
+    fill_in 'bid_line_item_bids_attributes_3_unit_cost', :with => 'x'
+    fill_in 'bid_line_item_bids_attributes_4_unit_cost', :with => 'x'
+    click_button "Publish Bid"
+    find('.alert-message').should have_content('errors prohibited this bid from being saved:')
+    
+    /* Test Case 2 (User Cancels Bid) /*
     click_link "Browse"
     click_link "Developer B Project 5"
     click_link "Enter Bid"
@@ -44,8 +31,43 @@ fixtures:all
     fill_in 'bid_line_item_bids_attributes_1_unit_cost', :with => '400'
     fill_in 'bid_line_item_bids_attributes_2_unit_cost', :with => '300'
     fill_in 'bid_line_item_bids_attributes_3_unit_cost', :with => '200'
-    fill_in 'bid_line_item_bids_attributes_0_unit_cost', :with => '100'
+    fill_in 'bid_line_item_bids_attributes_4_unit_cost', :with => '100'
+    click_button "Cancel Bid"
+    find('.alert-message').should have_content('Bid was successfully created')
+
+     /* Test Case 3 (User Saves Bid as Draft) /*
+    click_link "Browse"
+    click_link "Developer B Project 5"
+    click_link "Enter Bid"
+    fill_in 'bid_line_item_bids_attributes_0_unit_cost', :with => '500'
+    fill_in 'bid_line_item_bids_attributes_1_unit_cost', :with => '400'
+    fill_in 'bid_line_item_bids_attributes_2_unit_cost', :with => '300'
+    fill_in 'bid_line_item_bids_attributes_3_unit_cost', :with => '200'
+    fill_in 'bid_line_item_bids_attributes_4_unit_cost', :with => '100'
+    click_button "Save Bid"
+    find('.alert-message').should have_content('Bid was successfully created as Draft.')
+
+     /* Test Case 4 (User Publishes Bid - from Bid Creation) /*
+    click_link "Browse"
+    click_link "Developer B Project 5"
+    click_link "Enter Bid"
+    fill_in 'bid_line_item_bids_attributes_0_unit_cost', :with => '500'
+    fill_in 'bid_line_item_bids_attributes_1_unit_cost', :with => '400'
+    fill_in 'bid_line_item_bids_attributes_2_unit_cost', :with => '300'
+    fill_in 'bid_line_item_bids_attributes_3_unit_cost', :with => '200'
+    fill_in 'bid_line_item_bids_attributes_4_unit_cost', :with => '100'
     click_button "Publish Bid"
     find('.alert-message').should have_content('Bid was successfully created')
+   
+     /* Test Case 5 (User Publishes Bid - from Draft) /*
+    click_link "Browse"
+    click_link "Developer B Project 5"
+    click_link "Edit Bid"
+    fill_in 'bid_line_item_bids_attributes_0_unit_cost', :with => '500'
+    fill_in 'bid_line_item_bids_attributes_1_unit_cost', :with => '400'
+    fill_in 'bid_line_item_bids_attributes_2_unit_cost', :with => '300'
+    fill_in 'bid_line_item_bids_attributes_3_unit_cost', :with => '200'
+    fill_in 'bid_line_item_bids_attributes_4_unit_cost', :with => '100'
+    click_button "Publish Bid"
     end
  end
