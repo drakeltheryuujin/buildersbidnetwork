18ffe9549151c2eb74c5b5c9cd97eb7b2f06a0eb
micah craig <micah@micahcraig.net>
2012-05-04 11:30:19 -0400

    put_if_current_page_and_param helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,6 +3,10 @@ module HomeHelper
     current_page?(path) ? output : ''
   end
 
+  def put_if_current_page_and_param(output, path, filter = :filter, value = nil)
+    (current_page?(path) && value.to_s.eql?(params[filter.to_s].to_s)) ? output : ''
+  end
+
   def projects_posted_in_days_count(days = 30)
     Project.where(:state => :published.to_s).where("updated_at > :then", :then => (Time.now - days.days)).count
   end
